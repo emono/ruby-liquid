a9e33c867a4d8a8e9ad180dd5f1f42faf773951f
emono <ababababa0222@gmail.com>
2019-06-05 23:56:06 +0900

    fix spec

--- spec/liquid_spec.rb
+++ spec/liquid_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Liquid do
   it "has a version number" do
-    expect(Liquid::VERSION).to eq("0.1.1")
+    expect(Liquid::VERSION).to eq("0.1.2")
   end
 
   context ".product_detail" do
